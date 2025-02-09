299465c2cb050e9dbf5b98d9b7c20c209f4b2036
Kaleche <kaleche@kale.home>
2025-02-09 02:22:01 +0000

    need to hook in the model and declare the label encoders(if needed)

--- WEBSITE_FOR_TEAM_GRACE/api.py
+++ WEBSITE_FOR_TEAM_GRACE/api.py
@@ -73,4 +73,4 @@ def predict():
     return jsonify({"risk_score": round(risk_score, 2), "severity": severity})
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=6000)
+    app.run(debug=True, host="0.0.0.0", port=5000)
--- WEBSITE_FOR_TEAM_GRACE/app.py
+++ WEBSITE_FOR_TEAM_GRACE/app.py
@@ -1,6 +1,14 @@
 import streamlit as st
 import requests
 
+#get the host
+import os
+host = "127.0.0.1"
+try:
+    host = os.environ['DockHost']
+except:
+    host = "127.0.0.1"
+
 st.title("🚦 Real-Time Accident Risk Predictor")
 st.write("Enter a location and time to get an accident risk score.")
 
@@ -8,7 +16,7 @@ location = st.text_input("Enter location (City, Country):")
 hour = st.slider("Select Hour of the Day", 0, 23, 12)
 
 if st.button("Predict Risk"):
-    response = requests.get(f"http://back:6000/predict?location={location}&hour={hour}")
+    response = requests.get(f"http://{host}:5000/predict?location={location}&hour={hour}")
     if response.status_code == 200:
         result = response.json()
         st.success(f"Predicted Risk Score: {result['risk_score']}%")
